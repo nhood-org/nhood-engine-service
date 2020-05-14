bdaa8a7b6e6e02d21fe12eeb6a394241eaf0d2f8
Krzysztof Reczek <reczek.krzysztof@gmail.com>
2020-05-14 11:37:56 +0200

    Update nhood-engine-service-app/src/main/java/com/h8/nh/service/app/query/FindClosesDataQueryHandler.java

--- nhood-engine-service-app/src/main/java/com/h8/nh/service/app/query/FindClosesDataQueryHandler.java
+++ nhood-engine-service-app/src/main/java/com/h8/nh/service/app/query/FindClosesDataQueryHandler.java
@@ -4,11 +4,11 @@ import com.h8.nh.service.app.engine.EngineDataFinder;
 
 public class FindClosesDataQueryHandler {
 
-    private final EngineDataFinder engineDataFinder;
-
-    public FindClosesDataQueryHandler(EngineDataFinder engineDataFinder) {
-        this.engineDataFinder = engineDataFinder;
-    }
+//    private final EngineDataFinder engineDataFinder;
+//
+//    public FindClosesDataQueryHandler(EngineDataFinder engineDataFinder) {
+//        this.engineDataFinder = engineDataFinder;
+//    }
 
     public FindClosestDataQueryResult handle(FindClosestDataQuery query) {
         // TODO!!! implement me
