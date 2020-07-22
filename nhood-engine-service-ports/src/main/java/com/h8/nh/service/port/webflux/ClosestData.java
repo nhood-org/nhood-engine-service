984dabda00897360dd21599fcb9dcb7f40f3b026
krzysztofreczek <reczek.krzysztof@gmail.com>
2020-07-22 21:08:52 +0200

    Fix failing tests

--- nhood-engine-service-ports/src/main/java/com/h8/nh/service/port/webflux/ClosestData.java
+++ nhood-engine-service-ports/src/main/java/com/h8/nh/service/port/webflux/ClosestData.java
@@ -4,25 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Arrays;
-
 @NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class ClosestData {
-    private int[] data;
-
-    public ClosestData(int[] data) {
-        this.data = copy(data);
-    }
-
-    public int[] getData() {
-        return copy(data);
-    }
-
-    public void setData(int[] data) {
-        this.data = copy(data);
-    }
-
-    private int[] copy(int[] d) {
-        return Arrays.copyOf(d, d.length);
-    }
+    private int data;
 }
--- nhood-engine-service-ports/src/main/java/com/h8/nh/service/port/webflux/ClosestDataFinderWebFluxAdapter.java
+++ nhood-engine-service-ports/src/main/java/com/h8/nh/service/port/webflux/ClosestDataFinderWebFluxAdapter.java
@@ -1,36 +1,19 @@
 package com.h8.nh.service.port.webflux;
 
-import com.h8.nh.nhoodengine.core.DataResource;
-import com.h8.nh.nhoodengine.matrix.DataMatrixRepositoryFailedException;
-import com.h8.nh.service.app.engine.*;
-import com.h8.nh.service.app.query.FindClosestDataQuery;
-import com.h8.nh.service.app.query.FindClosesDataQueryHandler;
+import com.h8.nh.service.app.query.NoopFindClosestDataQuery;
+import com.h8.nh.service.app.query.NoopFindClosestDataQueryHandler;
 import reactor.core.publisher.Mono;
 
 public class ClosestDataFinderWebFluxAdapter implements ClosestDataFinder {
-    public static final int RESULT_SIZE = 5;
 
-    private final EngineDataRepository repository = new EngineDataRepositoryImpl(EngineDataResourceKey.METADATA_SIZE);
-    private final EngineDataFinder finder = new EngineDataFinderImpl(repository);
-    private final FindClosesDataQueryHandler handler = new FindClosesDataQueryHandler(finder);
+    private final NoopFindClosestDataQueryHandler handler = new NoopFindClosestDataQueryHandler();
 
-    public ClosestDataFinderWebFluxAdapter() {
-        try {
-            for (int i = 0; i < 100; i++) {
-                repository.add(DataResource.<EngineDataResourceKey, EngineData>builder()
-                        .key(new EngineDataResourceKey(i))
-                        .data(new EngineData(i))
-                        .build());
-            }
-        } catch (DataMatrixRepositoryFailedException e) {
-                System.out.println(e);
-        }
-    }
+    public ClosestDataFinderWebFluxAdapter() {}
 
     @Override
     public Mono<ClosestData> findByID(int id) {
-        var query = new FindClosestDataQuery(id, RESULT_SIZE);
+        var query = new NoopFindClosestDataQuery(id);
         var result = handler.handle(query);
-        return Mono.just(new ClosestData(result.getResults()));
+        return Mono.just(new ClosestData(result.getId()));
     }
 }
